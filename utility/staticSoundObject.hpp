63d01506aee7e3230e19ecdce6c5ee2e779b6a72
Lucian <lucianparisi@gmail.com>
2025-06-02 17:33:42 -0700

    added sub

--- utility/staticSoundObject.hpp
+++ utility/staticSoundObject.hpp
@@ -26,6 +26,8 @@ public:
       float sampleValue = mSource() * 0.3; // temporary fix
       io.out(0) += sampleValue;
       io.out(1) += sampleValue;
+
+      io.out(47) += sampleValue;
     }
     if (mSource.done()) {
       free();
